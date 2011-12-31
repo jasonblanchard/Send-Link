6225e0178fa0302a6276dffdbb96534b42f3c6b0
Jason Blanchard <jas.blanchard@gmail.com>
2011-12-31 11:31:17 -0500

    Added comments to linkff.sh

--- linkff.sh
+++ linkff.sh
@@ -2,8 +2,8 @@
 
 #You can create an link to this in ~/bin to execute it from the command line as part of your $PATH variable
 
-# change {username} to your user name. Create the linkapp directory and make sure
-#link.txt is in there.
+# change {username} to your user name with no curly brackets.
+#Create the ~/linkapp directory and make sure link.txt is in there.
 link=`cat /home/{username}/linkapp/link.txt`
 
 firefox "$link" 
