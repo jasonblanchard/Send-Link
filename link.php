d0b45312ab42bd0f24c5eb5d2549de60d67d76fc
Jason Blanchard <jas.blanchard@gmail.com>
2011-12-31 11:21:11 -0500

    Added the php and bash files

---
+++ link.php
@@ -0,0 +1,35 @@
+<html>
+<head></head>
+<body>
+
+<!-- Put this in your apache document root to access it over your local network. You may need to change some information below to make it link to the correct files and directories in ~/linkapp -->
+
+Enter link URL here to view on the TV:
+<br />
+
+<form action="link.php" method="post">
+<input type="text" name="url" />
+<input type="submit" value="Watch on TV" />
+</form>
+
+<?php
+$url=$_POST['url'];
+# Change {username} to your username
+$linkfile="/home/{username}/linkapp/link.txt";
+$fh=fopen($linkfile,'w') or die("Can't open file");
+
+if (isset($url)) {
+        #writes it to a file so you can execute it with the linkff.sh script
+        fwrite($fh,$url);
+        fclose($fh);
+        #Safely executes in firefox. Make sure you have changed the settings for www-data user. See README for more info. Change {username} to your user name.
+        $escaped_command=escapeshellcmd("sudo -u {username} firefox");
+        exec("$escaped_command --display :0.0 $url > /dev/null 2>/dev/null &");
+}
+
+?>
+
+</body>
+</html>
+
+
---
+++ linkff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#You can create an link to this in ~/bin to execute it from the command line as part of your $PATH variable
+
+# change {username} to your user name. Create the linkapp directory and make sure
+#link.txt is in there.
+link=`cat /home/{username}/linkapp/link.txt`
+
+firefox "$link" 
